c53b58313e8ec1af6b9f7f2cd4a399d5fd9d439e
Julija <julija.pesticek@student.um.si>
2023-07-28 20:18:43 +0200

    Added two cards with another 3D models.

--- src/components/ModelViewer.js
+++ src/components/ModelViewer.js
@@ -10,15 +10,60 @@ function Model(props) {
 
 function ModelViewer() {
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '85vh' }}>
-            {/* First Card */}
-            <Card style={{ width: '20rem', border: '1px solid #dee2e6', borderRadius: '8px', marginRight: '20px' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', minHeight: '85vh' }}>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '30px' }}>
+                {/* First Card */}
+                <Card style={{ width: '20rem', border: '1px solid #dee2e6', borderRadius: '8px', marginRight: '20px' }}>
+                    <div style={{ width: '100%', height: '300px', position: 'relative' }}>
+                        <Canvas dpr={[1, 2]} shadows={'basic'} camera={{ fov: 45 }} style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
+                            <color attach="background" args={['#00000']} />
+                            <PresentationControls speed={1.5} global zoom={.5}>
+                                <Stage environment={'studio'}>
+                                    <Model modelFile="/Poskus6.glb" scale={0.02}  rotation={[0, Math.PI, 0]} />
+                                </Stage>
+                            </PresentationControls>
+                        </Canvas>
+                    </div>
+                    <Card.Body style={{ marginTop: '10px', marginLeft: '10px', marginBottom: '10px' }}>
+                        <Card.Title>3D model of a t-shirt</Card.Title>
+                        <Card.Text>
+                            Feel free to drag around the model.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+
+                {/* Second Card */}
+                <Card style={{ width: '20rem', border: '1px solid #dee2e6', borderRadius: '8px' }}>
+                    <div style={{ width: '100%', height: '300px', position: 'relative' }}>
+                        <Canvas dpr={[1, 2]} shadows={'basic'} camera={{ fov: 45 }} style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
+                            <color attach="background" args={['#00000']} />
+                            <PresentationControls speed={1.5} global zoom={.5}>
+                                <Stage environment={'studio'}>
+                                    <Model modelFile="/Poskus5.glb" scale={0.02}  rotation={[0, Math.PI, 0]} />
+                                </Stage>
+                            </PresentationControls>
+                        </Canvas>
+                    </div>
+                    <Card.Body style={{ marginTop: '10px', marginLeft: '10px', marginBottom: '10px' }}>
+                        <Card.Title>3D model of a t-shirt</Card.Title>
+                        <Card.Text>
+                            Feel free to drag around the model.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+            </div>
+
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '30px', marginBottom: '30px' }}>
+                {/* Third Card */}
+                <Card style={{ width: '20rem', border: '1px solid #dee2e6', borderRadius: '8px', marginRight: '20px' }}>
                 <div style={{ width: '100%', height: '300px', position: 'relative' }}>
                     <Canvas dpr={[1, 2]} shadows={'basic'} camera={{ fov: 45 }} style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
                         <color attach="background" args={['#00000']} />
                         <PresentationControls speed={1.5} global zoom={.5}>
                             <Stage environment={'studio'}>
-                                <Model modelFile="/Poskus5.glb" scale={0.02}  rotation={[0, Math.PI, 0]} />
+                                <Model modelFile="/Poskus4.glb" scale={0.02} rotation={[0, Math.PI, 0]} />
                             </Stage>
                         </PresentationControls>
                     </Canvas>
@@ -30,28 +75,29 @@ function ModelViewer() {
                     </Card.Text>
                     <Button variant="primary">Go somewhere</Button>
                 </Card.Body>
-            </Card>
+                </Card>
 
-            {/* Second Card */}
-            <Card style={{ width: '20rem', border: '1px solid #dee2e6', borderRadius: '8px' }}>
-                <div style={{ width: '100%', height: '300px', position: 'relative' }}>
-                    <Canvas dpr={[1, 2]} shadows={'basic'} camera={{ fov: 45 }} style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
-                        <color attach="background" args={['#00000']} />
-                        <PresentationControls speed={1.5} global zoom={.5}>
-                            <Stage environment={'studio'}>
-                                <Model modelFile="/Poskus4.glb" scale={0.02}  rotation={[0, Math.PI, 0]} />
-                            </Stage>
-                        </PresentationControls>
-                    </Canvas>
-                </div>
-                <Card.Body style={{ marginTop: '10px', marginLeft: '10px', marginBottom: '10px' }}>
-                    <Card.Title>3D model of another object</Card.Title>
-                    <Card.Text>
-                        Feel free to drag around the model.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-            </Card>
+                {/* Fourth Card */}
+                <Card style={{ width: '20rem', border: '1px solid #dee2e6', borderRadius: '8px' }}>
+                    <div style={{ width: '100%', height: '300px', position: 'relative' }}>
+                        <Canvas dpr={[1, 2]} shadows={'basic'} camera={{ fov: 45 }} style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
+                            <color attach="background" args={['#00000']} />
+                            <PresentationControls speed={1.5} global zoom={.5}>
+                                <Stage environment={'studio'}>
+                                    <Model modelFile="/Poskus7.glb" scale={0.02} rotation={[0, Math.PI, 0]} />
+                                </Stage>
+                            </PresentationControls>
+                        </Canvas>
+                    </div>
+                    <Card.Body style={{ marginTop: '10px', marginLeft: '10px', marginBottom: '10px' }}>
+                        <Card.Title>3D model of a t-shirt</Card.Title>
+                        <Card.Text>
+                            Feel free to drag around the model.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+            </div>
         </div>
     );
 }
